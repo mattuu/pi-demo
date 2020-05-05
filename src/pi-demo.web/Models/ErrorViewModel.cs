55bfadc423bb896015ff6f66c0ebd33c72b6987e
mattuu <maciej@uliasz.com>
2020-05-05 19:29:16 +0100

    Initial commit

---
+++ src/pi-demo.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pi_demo.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
